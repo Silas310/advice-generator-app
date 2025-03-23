c3e88a1ed705a61d70564a160e850193b85b3089
Silas <silascosta310@gmail.com>
2025-03-23 07:43:47 -0300

    chore: set colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <main className='main-content'>
-      <h1>oi</h1>
-      <p></p>
+      <h1 className='advice-number'>oi</h1>
+      <p className='advice'>lorem</p>
       <picture>
         <source media="(min-width: 1440px)" srcset="./images\pattern-divider-desktop.svg" />
         <img src="./images\pattern-divider-mobile.svg" alt="divider row" />
