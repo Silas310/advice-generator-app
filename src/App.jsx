10115cd6913019efcaa7b96dc3962fc192db9dfa
Silas <silascosta310@gmail.com>
2025-03-23 08:06:38 -0300

    feat: add initiaul layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <main className='main-content'>
-      <h1 className='advice-number'>oi</h1>
-      <p className='advice'>lorem</p>
+      <h1 className='advice-number'>ADVICE #117</h1>
+      <p className='advice'>"It's easy to sit up and take notice, what's difficult is getting up and taking action."</p>
       <picture>
         <source media="(min-width: 1440px)" srcset="./images\pattern-divider-desktop.svg" />
         <img src="./images\pattern-divider-mobile.svg" alt="divider row" />
