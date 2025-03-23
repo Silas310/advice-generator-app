b7b3595b653e8be2894f69d3c5dd258e645db974
Silas <silascosta310@gmail.com>
2025-03-23 08:37:39 -0300

    feat: adjust layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
     <main className='main-content'>
       <h1 className='advice-number'>ADVICE #117</h1>
       <p className='advice'>"It's easy to sit up and take notice, what's difficult is getting up and taking action."</p>
-      <picture>
-        <source media="(min-width: 1440px)" srcset="./images\pattern-divider-desktop.svg" />
+      <picture className='pic'>
+        <source media="(min-width: 720px)" srcset="./images\pattern-divider-desktop.svg" />
         <img src="./images\pattern-divider-mobile.svg" alt="divider row" />
       </picture>
       <button className='button'></button>
